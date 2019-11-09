be7996b2aeb27728d3d1f95b1944eb9ed7073d28
nchern <nchern@inbox.ru>
2019-11-09 17:39:56 +0100

    iter: adds comments to public methods

--- pkg/generic/iterator/iterator.go
+++ pkg/generic/iterator/iterator.go
@@ -5,7 +5,10 @@ type T0 string
 type T0GeneratorFunc func(generator chan<- T0) error
 
 type T0Iter interface {
+	// Err returns error if it happened during generation
 	Err() error
+
+	// Next returns next element from iterator
 	Next() <-chan T0
 }
 
@@ -14,14 +17,17 @@ type iter struct {
 	iter chan T0
 }
 
+// Err returns error if it happened during generation
 func (i *iter) Err() error {
 	return i.err
 }
 
+// Next returns next element from iterator
 func (i *iter) Next() <-chan T0 {
 	return i.iter
 }
 
+// Generate creates Iterator from generator func
 func Generate(f T0GeneratorFunc) T0Iter {
 	gen := &iter{
 		iter: make(chan T0),
